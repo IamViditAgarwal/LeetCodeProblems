0b21e7bd8ea2391c6391fd5a2c00630a3cba3624
IamViditAgarwal <viditvivo@gmail.com>
2020-06-05 13:52:12 +0530

    Added the solution for problem 9: Pallindrome Number

---
+++ 9-pallindrome_number/sol.py
@@ -0,0 +1,10 @@
+# Python2
+# Question : https://leetcode.com/problems/palindrome-number/
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return str(x) == str(x)[::-1]
+
